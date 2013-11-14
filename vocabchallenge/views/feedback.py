f5009f6d6e56c48c4e1f1162641f934fb47ee8f0
lzbotha <leonardzbotha@gmail.com>
2013-11-14 18:07:39 +0200

    feedback is working excluding database stuff

--- vocabchallenge/views/feedback.py
+++ vocabchallenge/views/feedback.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash
 from vocabchallenge import app
 
-@app.route('/feedback')
+@app.route('/feedback', methods=('post', 'get'))
 def feedback():
-	return render_template('feedback.html')
+	if 'feedback' in request.form:
+		feedback = request.form['feedback']
+		flash('Thank you for your feedback.')
+		
+		# add feedback to database
+
+		return redirect('/')
+	else:
+		return render_template('feedback.html')
