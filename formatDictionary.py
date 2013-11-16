adc20815d282e1311513ccb65ac54291efeebcc0
lzbotha <leonardzbotha@gmail.com>
2013-11-16 20:45:08 +0200

    created a python script to insert words and definitions into database

--- formatDictionary.py
+++ formatDictionary.py
@@ -1,7 +1,28 @@
 
+def normalchars(s):
+    for c in s:
+        if not 97 <= ord(c) <= 122 and c!=' ':
+            return False
+    return True
+
 f = open('TEMP-E20131101.tsv','r')
+outpt = open('dictionary.tsv','w')
+wordcount = 0
+prevword = ''
 for line in f:
-    dictitem = line.split('\t')[3]
-    if (65 <= ord(line[8:-1][0]) <= 90) or (97 <= ord(line[8:-1][0]) <= 122):
-        print dictitem
+    dictitem = line.split('\t')
+    #if the entry starts with a letter
+    if (97 <= ord(dictitem[1][0]) <= 122):
+        # eliminate all weird things like prefixes, suffixes, acronyms etc
+        if dictitem[2][0:2]!='{{' and dictitem[2]!='Prefix' and dictitem[2]!='Letter' and dictitem[2]!='Proper noun' and dictitem[2]!='Symbol' and dictitem[2]!='Abbreviation' and dictitem[2]!='Initialism' and dictitem[2]!='Preposition':
+            # filter out vulgar words and other things
+            if not 'vulgar' in dictitem[3] and not 'initialism' in dictitem[3] and not 'idiomatic' in dictitem[3] and not 'abbreviation' in dictitem[3]:
+                # remove duplicates... this is maybe not a good call
+                if prevword != dictitem[1]:
+                    # check that words only contain characters a-z
+                    if normalchars(dictitem[1]):
+                        outpt.write(line)
+                        prevword = dictitem[1]
+                        wordcount+=1
 f.close()
+print wordcount
--- vocabchallenge/views/game_ended.py
+++ vocabchallenge/views/game_ended.py
@@ -3,4 +3,5 @@ from vocabchallenge import app
 
 @app.route('/game_ended')
 def game_ended():
+    #this needs to query the database for a users past scores and shit
     return render_template('game_ended.html')
