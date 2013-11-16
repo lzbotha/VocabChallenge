977104f011a7d3e7bbb48de9ef44b811b813de8a
lzbotha <leonardzbotha@gmail.com>
2013-11-16 18:41:38 +0200

    more str formatting

--- formatDictionary.py
+++ formatDictionary.py
@@ -1,6 +1,7 @@
 
 f = open('TEMP-E20131101.tsv','r')
 for line in f:
+    dictitem = line.split('\t')[3]
     if (65 <= ord(line[8:-1][0]) <= 90) or (97 <= ord(line[8:-1][0]) <= 122):
-        print line[8:-1]
+        print dictitem
 f.close()
