729244fa8eba9ad1aadef5e76a025e6ab373a7b2
lzbotha <leonardzbotha@gmail.com>
2013-11-17 21:54:35 +0200

    implemented more game logic

--- vocabchallenge/views/game.py
+++ vocabchallenge/views/game.py
@@ -1,46 +1,45 @@
 from flask import Flask, render_template, redirect, request, session
 from vocabchallenge import app, database
 
-
-# def game():
-#     # if the game is lost it needs to redirect to game_ended
-#     if True:
-#         word, definition = database.get_entry(0)
-#         if 'wordguess' in request.form:
-#             wordguess = request.form['wordguess']
-
-#             # player guessed the word right
-#             if wordguess == word:
-#                 # could prob stick the users current score is session
-#                 print 'well done'
-#             return redirect('/game')
-#         else:
-#             return render_template('game.html', word=word, definition=definition)
-#     else:
-#         redirect('/game_ended')
-
-#session['logged_in'] = True
 @app.route('/game', methods=('post', 'get'))
 def game():
-    print 'word' in session
-    if 'word' in session:
+    
+    if session.pop('ingame',False):
 
-        #user has submitted feedback
-        if 'wordguess' in request.form:
-            wordguess = request.form['wordguess']
+        #word and definition retrieved from database
+        if 'word' in session:
 
-            #user has guessed the word correctly
-            if wordguess == session.pop('word'):
-                return redirect('/game')
+            #user has submitted feedback
+            if 'wordguess' in request.form:
+                wordguess = request.form['wordguess']
 
-            #user has incorrectly guessed the word
+                #user has guessed the word correctly
+                if wordguess == session.pop('word'):
+                    session['ingame'] = True
+                    session['score'] += 1
+                    return redirect('/game')
+
+                #user has incorrectly guessed the word
+                else:
+                    if session['lives'] > 0:
+                        session['lives'] -= 1
+                        session['ingame'] = True
+                        return redirect('/game')
+                    return redirect('/game_ended')
+
+            #user has not yet submitted feedback
             else:
-                return redirect('/game_ended')
-
-        #user has not yet submitted feedback
+                session['ingame'] = True
+                return render_template('game.html', word=session['word'], definition=session['definition'])
+        
+        #word and definition not yet retrieved from database
         else:
+            session['ingame'] = True
+            session['word'], session['definition'] = database.get_entry(0)
             return render_template('game.html', word=session['word'], definition=session['definition'])
+
     else:
-        session['word'], session['definition'] = database.get_entry(0)
-        print (session['word'], session['definition'])
-        return render_template('game.html', word=session['word'], definition=session['definition'])
+        session['ingame'] = True
+        session['score'] = 0
+        session['lives'] = 3
+        return redirect('/game')
