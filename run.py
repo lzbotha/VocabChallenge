7319646d27ca0b6d819144f532f4a6acbd27f482
lzbotha <leonardzbotha@gmail.com>
2013-11-25 10:04:53 +0200

    reconnected hosting stuff

--- run.py
+++ run.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
     # if app.config['SENTRY_ENABLED']:
     #     sentry = Sentry(app, dsn=app.config['DATABASE_NAME'])
 
-    app.run()
-    # print 'Running in production mode'
+    # app.run()
+    print 'Running in production mode'
 
-    #figure out argparser you lazy twat
+    # figure out argparser you lazy twat
 
-    #app.run(debug=True, host='0.0.0.0', port=args.port, use_debugger=True)
-    # http_server = HTTPServer(WSGIContainer(app))
-    # http_server.listen(8080, address='0.0.0.0')
-    # IOLoop.instance().start()
+    app.run(debug=True, host='0.0.0.0', port=args.port, use_debugger=True)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(8080, address='0.0.0.0')
+    IOLoop.instance().start()
