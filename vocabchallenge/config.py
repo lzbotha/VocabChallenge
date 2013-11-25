b61d84a97203bf2a3399473403e2ce3d0639a9f4
lzbotha <leonardzbotha@gmail.com>
2013-11-25 20:40:08 +0200

    added mxit_ids

--- vocabchallenge/config.py
+++ vocabchallenge/config.py
@@ -5,7 +5,10 @@ DATABASE_USER = 'mxitapps'
 DATABASE_PASSWORD = 'test123'
 DATABASE_NAME = 'vocabchallenge'
 DEBUG = True
-SECRET_KEY = '4c91b78551f847d59264e8ea60890dfe'
+SECRET_KEY = 'developement id'
 SENTRY_DSN = ''
 SENTRY_ENABLED = False
 PORT = 80
+
+client_id = '95fbce1bb38e495ba76b9b21b38a5491'
+secret_id = '4c91b78551f847d59264e8ea60890dfe'
