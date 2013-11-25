c75a3d102b10be0cd05d9562ede02f46c7ea6cb4
lzbotha <leonardzbotha@gmail.com>
2013-11-25 10:10:24 +0200

    removed db password

--- vocabchallenge/database.py
+++ vocabchallenge/database.py
@@ -3,7 +3,7 @@ from flask import g, request, session
 from vocabchallenge import app
 
 def connect_db():
-    database = psycopg2.connect('dbname=%s host=%s user=%s password=%s' % (app.config['DATABASE_NAME'], app.config['DATABASE_HOST'] , app.config['DATABASE_USER'], app.config['DATABASE_PASSWORD']))
+    database = psycopg2.connect('dbname=%s host=%s user=%s' % (app.config['DATABASE_NAME'], app.config['DATABASE_HOST'] , app.config['DATABASE_USER']))
     return database
 
 def disconnect_db():
