c6e4ab5c2b853e5983467ecbcd34bdd92ce0fb42
lzbotha <leonardzbotha@gmail.com>
2013-12-01 19:20:58 +0200

    changed port

--- vocabchallenge/config.py
+++ vocabchallenge/config.py
@@ -8,7 +8,7 @@ DEBUG = True
 SECRET_KEY = 'developement id'
 SENTRY_DSN = ''
 SENTRY_ENABLED = False
-PORT = 80
+PORT = 10000
 
 client_id = '95fbce1bb38e495ba76b9b21b38a5491'
 secret_id = '4c91b78551f847d59264e8ea60890dfe'
