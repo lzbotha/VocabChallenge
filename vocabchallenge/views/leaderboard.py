a371265bc75a6d7fd1b0e7ca639c2a5038ef0daf
lzbotha <leonardzbotha@gmail.com>
2013-12-02 10:57:59 +0200

    added language tabs for leaderboard

--- vocabchallenge/views/leaderboard.py
+++ vocabchallenge/views/leaderboard.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, session
 from vocabchallenge import app, database
 
-@app.route('/leaderboard')
-def leaderboard():
+@app.route('/leaderboard/')
+@app.route('/leaderboard/<language>')
+def leaderboard(language=None):
     session.pop('ingame',None)
-    return render_template('leaderboard.html',top10=database.get_top(10))
+    if language:
+        return render_template('leaderboard.html',top10=database.get_top_by_language(10,language))
+    else:
+        return render_template('leaderboard.html',top10=database.get_top(10))
