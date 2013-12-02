9803f6da32aff93cdae8c8ed60360a6dcab1a860
lzbotha <leonardzbotha@gmail.com>
2013-12-02 10:43:53 +0200

    fixed sql query

--- vocabchallenge/database.py
+++ vocabchallenge/database.py
@@ -135,7 +135,7 @@ def get_highscore(userid):
 
 def get_top(x):
     cur = g.database.cursor()
-    cur.execute('SELECT users.username, MAX(scores.score) AS highscore, language FROM scores, users WHERE users.id=scores.userid GROUP BY users.username ORDER BY highscore DESC LIMIT %s', [x])
+    cur.execute('SELECT users.username, MAX(scores.score) AS highscore, scores.language FROM scores, users WHERE users.id=scores.userid GROUP BY users.username ORDER BY highscore DESC, scores.language DESC  LIMIT %s', [x])
     topx = [dict(username=row[0], score=str(row[1])+"\t"+row[2].capitalize()) for row in cur.fetchall()]
     cur.close()
     return topx
