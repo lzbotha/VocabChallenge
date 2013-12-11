3351aa155f786ad16ce6b057ce39292f659d0b40
lzbotha <leonardzbotha@gmail.com>
2013-12-11 10:01:30 +0200

    fixed mixpanel bug on index page

--- vocabchallenge/views/index.py
+++ vocabchallenge/views/index.py
@@ -4,5 +4,5 @@ from vocabchallenge import app, mp
 @app.route('/')
 def index():
     session.pop('ingame', None)
-    mp.track(session['username','index viewed'])
+    mp.track(session['username'],'index viewed')
     return render_template('index.html',username=session['username'])
