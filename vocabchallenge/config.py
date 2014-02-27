f2ede1ec22343b593df31ce456373b7c2c439102
bzl <leonardzbotha@gmail.com>
2014-02-27 15:14:58 +0200

    changed config

--- vocabchallenge/config.py
+++ vocabchallenge/config.py
@@ -2,11 +2,11 @@
 # configuration
 DATABASE_HOST = 'localhost'
 DATABASE_USER = 'mxitapps'
-DATABASE_PASSWORD = 'test123'
+DATABASE_PASSWORD = 'password'
 DATABASE_NAME = 'vocabchallenge'
 DEBUG = True
 SECRET_KEY = 'developement id'
 SENTRY_DSN = ''
 SENTRY_ENABLED = False
 PORT = 10000
-MIXPANEL_PROJECT_TOKEN = ''
+MIXPANEL_PROJECT_TOKEN = '1234'
